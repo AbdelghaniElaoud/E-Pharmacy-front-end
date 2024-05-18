9bbdccca728d6ea0f7d71a5e40bbb6f68ca9656f
Abdelghani ELAOUD <abdelghanielaoud@gmail.com>
2024-05-18 21:57:30 +0200

    Added cart component

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,26 +1,53 @@
 import { Component } from '@angular/core';
 import {CartService} from "../../service/cart-service/cart-service.service";
-import {NgForOf} from "@angular/common";
+import {KeyValuePipe, NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-cart',
   standalone: true,
   imports: [
-    NgForOf
+    NgForOf,
+    KeyValuePipe
   ],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css'
 })
 export class CartComponent {
-
-  cartProducts: any[] = [];
-  cartItemCount: number = 0;
+  cartItems: Map<any, number>;
+  totalPrice: number;
 
   constructor(private cartService: CartService) {
+    this.cartItems = this.cartService.cart;
+    this.totalPrice = this.cartService.totalPrice;
     this.cartService.cart$.subscribe(cart => {
-      this.cartItemCount = cart.length;
+      this.cartItems = cart;
+      this.totalPrice = this.cartService.totalPrice;
     });
-    this.cartProducts = this.cartService.getProducts();
   }
 
+  increaseQuantity(product: any): void {
+    const currentQuantity = this.cartItems.get(product) || 0;
+    this.cartItems.set(product, currentQuantity + 1);
+    this.cartService.increaseQuantity(product);
+  }
+
+  decreaseQuantity(product: any): void {
+    const currentQuantity = this.cartItems.get(product) || 0;
+    if (currentQuantity > 1) {
+      this.cartItems.set(product, currentQuantity - 1);
+    } else {
+      this.removeFromCart(product);
+    }
+    this.cartService.decreaseQuantity(product);
+  }
+
+  removeFromCart(product: any): void {
+    this.cartItems.delete(product);
+    this.cartService.removeFromCart(product);
+  }
+
+  isQuantityOne(product: any): boolean {
+    const currentQuantity = this.cartItems.get(product) || 0;
+    return currentQuantity === 1;
+  }
 }
--- src/app/service/cart-service/cart-service.service.ts
+++ src/app/service/cart-service/cart-service.service.ts
@@ -5,23 +5,54 @@ import {BehaviorSubject} from "rxjs";
   providedIn: 'root'
 })
 export class CartService {
-  private cart: any[] = [];
-  private cartSubject = new BehaviorSubject<any[]>(this.cart);
+  cart: Map<any, number> = new Map();
+  private cartSubject = new BehaviorSubject<Map<any, number>>(this.cart);
   cart$ = this.cartSubject.asObservable();
-
-  private products: any[] = [];
+  totalPrice: number = 0;
 
   addToCart(product: any) {
-    this.cart.push(product);
-    this.products.push(product);
+    const existingQuantity = this.cart.get(product) || 0;
+    this.cart.set(product, existingQuantity + 1);
+    this.updateTotalPrice();
     this.cartSubject.next(this.cart);
   }
 
-  getCartLength() {
-    return this.cart.length;
+  increaseQuantity(product: any) {
+    const currentQuantity = this.cart.get(product) || 0;
+    this.cart.set(product, currentQuantity + 1);
+    this.updateTotalPrice();
+    this.cartSubject.next(this.cart);
+  }
+
+  decreaseQuantity(product: any) {
+    const currentQuantity = this.cart.get(product) || 0;
+    if (currentQuantity > 1) {
+      this.cart.set(product, currentQuantity - 1);
+      this.updateTotalPrice();
+      this.cartSubject.next(this.cart);
+    } else {
+      this.removeFromCart(product);
+    }
+  }
+
+  removeFromCart(product: any) {
+    this.cart.delete(product);
+    this.updateTotalPrice();
+    this.cartSubject.next(this.cart);
   }
 
   getProducts() {
-    return this.products;
+    return [...this.cart.keys()];
+  }
+
+  getCartLength() {
+    return this.cart.size;
+  }
+
+  private updateTotalPrice() {
+    this.totalPrice = 0;
+    for (const [product, quantity] of this.cart.entries()) {
+      this.totalPrice += product.price * quantity;
+    }
   }
 }
