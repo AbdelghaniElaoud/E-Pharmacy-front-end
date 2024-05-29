152e9c6e924de7f6be2993bd3472805f4afeea21
Abdelghani ELAOUD <abdelghanielaoud@gmail.com>
2024-05-29 21:41:28 +0200

    Minor changes in the profile section.

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import {ProfileService} from "../../service/profile/profile.service";
-import {DatePipe, NgIf} from "@angular/common";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-profile',
@@ -9,13 +9,14 @@ import {DatePipe, NgIf} from "@angular/common";
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css'],
   imports: [
-    DatePipe,
     NgIf
   ],
   providers: [ProfileService]
 })
 export class ProfileComponent implements OnInit {
   profile: any;
+  accountAgeDays: number | null = null;
+  accountAgeHours: number | null = null;
 
   constructor(
     private profileService: ProfileService,
@@ -27,10 +28,19 @@ export class ProfileComponent implements OnInit {
     this.profileService.getProfile(userId).subscribe(
       data => {
         this.profile = data;
+        this.calculateAccountAge(this.profile.createdAt);
       },
       error => {
         console.error('Error fetching profile', error);
       }
     );
+  }
+
+  calculateAccountAge(createdAt: string): void {
+    const createdDate = new Date(createdAt);
+    const currentDate = new Date();
+    const diffTime = Math.abs(currentDate.getTime() - createdDate.getTime());
+    this.accountAgeDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
+    this.accountAgeHours = Math.floor(diffTime / (1000 * 60 * 60));
   }
 }
