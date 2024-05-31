1fe19e52392b9eef2460524db392085e590710ec
Abdelghani ELAOUD <abdelghanielaoud@gmail.com>
2024-05-31 02:36:10 +0200

    Added quick search in the users table.

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,32 +1,42 @@
-import {HttpClient, HttpClientModule} from "@angular/common/http";
-import {Router} from "@angular/router";
-import {Login} from "../../model/Login";
+import { HttpClient, HttpClientModule } from "@angular/common/http";
+import { Router } from "@angular/router";
+import { Login } from "../../model/Login";
 import { jwtDecode } from 'jwt-decode';
-import {Component} from "@angular/core";
-import {FormsModule} from "@angular/forms";
+import { Component } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import {NgIf} from "@angular/common";
 
 interface User {
   sub: string;
   id: number;
   email: string;
   roles: { authority: string }[];
-  active : boolean;
+  active: boolean;
 }
+
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule,HttpClientModule],
+  imports: [FormsModule, HttpClientModule, NgIf],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 
 export class LoginComponent {
-  LoginObj : Login;
+  LoginObj: Login;
+  RegisterObj: any = {
+    username: '',
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    phone: ''
+  };
+  showModal: boolean = false;
 
-  constructor(private http : HttpClient, private router:Router) {
+  constructor(private http: HttpClient, private router: Router) {
     this.LoginObj = new Login();
   }
-
 
   onLogin() {
     this.http.post('http://localhost:8080/api/auth/signin', this.LoginObj).subscribe((res: any) => {
@@ -37,26 +47,7 @@ export class LoginComponent {
           const decodedToken: User = jwtDecode(token);
           if (decodedToken.active) {
             const userRole = decodedToken.roles[0].authority;
-            this.router.navigate(['/dashboard'])
-            /*
-            switch (userRole) {
-              case 'ROLE_CUSTOMER':
-                this.router.navigate(['/customer']);
-                break;
-              case 'ROLE_ADMIN':
-                this.router.navigate(['/admin']);
-                break;
-              case 'ROLE_PHARMACIST':
-                this.router.navigate(['/pharmacist']);
-                break;
-              case 'ROLE_DELIVERY_MAN':
-                this.router.navigate(['/delivery-man']);
-                break;
-              default:
-                this.router.navigate(['/login']);
-                break;
-            }
-            */
+            this.router.navigate(['/dashboard']);
           } else {
             alert(res.message);
           }
@@ -68,8 +59,40 @@ export class LoginComponent {
       }
     });
   }
+
+  onRegister() {
+    const registerPayload = {
+      ...this.RegisterObj,
+      role: ['customer']  // Default role for registration
+    };
+
+    this.http.post('http://localhost:8080/api/auth/signup', registerPayload).subscribe((res: any) => {
+      if (res.success) {
+        alert('Registration successful! Please login.');
+        this.closeRegisterModal();
+      } else {
+        alert(res.message);
+      }
+    });
+  }
+
+  openRegisterModal() {
+    this.RegisterObj = {
+      username: '',
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: '',
+      phone: ''
+    };
+    this.showModal = true;
+  }
+
+  closeRegisterModal() {
+    this.showModal = false;
+  }
+
   isLoggedIn(): boolean {
     return !!localStorage.getItem('token');
   }
 }
-
