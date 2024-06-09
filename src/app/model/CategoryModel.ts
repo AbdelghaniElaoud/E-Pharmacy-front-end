60956ac74ffc5eca8b2c9ce71438cb2ea39beebc
Abdelghani ELAOUD <abdelghanielaoud@gmail.com>
2024-06-09 22:37:53 +0200

    Customizing the landing page to show three products.

--- src/app/model/CategoryModel.ts
+++ src/app/model/CategoryModel.ts
@@ -1,9 +1,11 @@
 export class CategoryModel {
   id: number;
   name: string;
+  productNumbers: number;
 
   constructor() {
     this.id = 0;
     this.name = '';
+    this.productNumbers = 0;
   }
 }
